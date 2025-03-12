322c583fc8e6a12370ca0b2bba346b6d15fc08e5
Dave2ooo <david_schweighofer@gmx.at>
2025-03-12 16:26:34 +0100

    added all subfolders of images

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -6,13 +6,13 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox
 
 // Precaching all files in a folder (e.g., `/images/`)
 workbox.routing.registerRoute(
-  ({ url }) => url.pathname.startsWith("images/"),
+  new RegExp("^images/.*"), // Matches everything inside /images/ and all subfolders
   new workbox.strategies.CacheFirst({
     cacheName: "images-cache",
     plugins: [
       new workbox.expiration.ExpirationPlugin({
-        maxEntries: 500, // Adjust based on your needs
-        purgeOnQuotaError: true, // Automatically delete old cache if storage is full
+        maxEntries: 500, // Adjust based on storage needs
+        purgeOnQuotaError: true, // Automatically remove old files if space is low
       }),
     ],
   })
