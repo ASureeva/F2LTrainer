138e64552bc613de9311928eb2021127359307d0
Dave2ooo <david_schweighofer@gmx.at>
2025-03-12 16:16:54 +0100

    added precaching

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -4,6 +4,21 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox
 
 // workbox.routing.registerRoute(({ request }) => request.destination === "image", new workbox.strategies.CacheFirst());
 
+// Precaching all files in a folder (e.g., `/images/`)
+workbox.routing.registerRoute(
+  ({ url }) => url.pathname.startsWith("/images/"),
+  new workbox.strategies.CacheFirst({
+    cacheName: "images-cache",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 500, // Adjust based on your needs
+        purgeOnQuotaError: true, // Automatically delete old cache if storage is full
+      }),
+    ],
+  })
+);
+
+// General caching strategy for other assets
 workbox.routing.registerRoute(
   ({ request }) => request.destination !== "",
   new workbox.strategies.StaleWhileRevalidate({
